5fd5b797bb20c5bf1dc47e78d200a775339f32b2
Dave Debreceni Jr <davidjr@debreceni.net>
2020-02-24 19:26:37 -0600

    Added some comments around the JavaScript file

--- Client-Side-Scripting/Review/index.js
+++ Client-Side-Scripting/Review/index.js
@@ -1,4 +1,9 @@
-// Question 1 Object
+// We can make objects by declaring a variable and creating a bunch of keys
+// and values in the form of 
+// var myObject = {
+//      key: answer
+// }
+
 var question1 = {
     question: "How many moons does Saturn have?",
     answer: 53
@@ -24,16 +29,28 @@ var question5 = {
     answer: "Kate"
 };
 
+// Define arrays for us to use.
 var questions = [question1, question2, question3, question4, question5];
 
 // For loops
-questions.forEach(question => {
-    askQuestion(question);
-});
+for(var i = 0; i < questions.length; i++) {
+    askQuestion(questions[i]);
+}
+// Or
+// questions.forEach(question => {
+//     askQuestion(question);
+// });
 
+
+// Create a function for us to use
 function askQuestion(question) {
+    // Get the element h3 by calling it via its id, then set its content to our question
     document.getElementById('question').textContent = question.question;
+
+    // Create a couple of pop-up prompts to ask the user for an answer
     var userAnswer = prompt('Answer: ', '');
+
+    // Right now we'll assume the answer to be exact
     if (userAnswer == question.answer) {
         alert('Correct');
     }
