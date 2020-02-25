abf3bd442738c3f63590ca42041c20f2b1edadc9
Hubol Persson-Gordon <hubol.gordon@gmail.com>
2020-02-25 08:29:13 -0600

    Some improvements to the review

--- Client-Side-Scripting/Review/index.js
+++ Client-Side-Scripting/Review/index.js
@@ -11,7 +11,7 @@ var question1 = {
 
 var question2 = {
     question: "Who sits on their phone all day long in class?",
-    answer: "Sebastien"
+    answer: "Sebastian"
 };
 
 var question3 = {
@@ -29,32 +29,74 @@ var question5 = {
     answer: "Kate"
 };
 
+var question6 = {
+    question: "Who wears this lovely (obnoxious) purple hat?",
+    answer: "Samuel"
+};
+
 // Define arrays for us to use.
-var questions = [question1, question2, question3, question4, question5];
+var questions = [question1, question2, question3, question4, question5, question6];
+// var questions = [question6];
 
-// For loops
-for(var i = 0; i < questions.length; i++) {
-    askQuestion(questions[i]);
-}
-// Or
-// questions.forEach(question => {
-//     askQuestion(question);
-// });
-
+currentQuestionIndex = 0;
 
 // Create a function for us to use
 function askQuestion(question) {
     // Get the element h3 by calling it via its id, then set its content to our question
     document.getElementById('question').textContent = question.question;
 
-    // Create a couple of pop-up prompts to ask the user for an answer
-    var userAnswer = prompt('Answer: ', '');
+    // // Create a couple of pop-up prompts to ask the user for an answer
+    // var userAnswer = prompt(`Answer: `, '');
 
-    // Right now we'll assume the answer to be exact
-    if (userAnswer == question.answer) {
-        alert('Correct');
+    // // Right now we'll assume the answer to be exact
+    // if (userAnswer == question.answer) {
+    //     alert('Correct');
+    // }
+    // else{
+    //     alert('Incorrect');
+    //     console.log(`Expected ${question.answer}`)
+    // }
+}
+
+inputField = document.getElementById("inputField");
+
+function answer()
+{
+    if (inputField.value == questions[currentQuestionIndex].answer)
+    {
+        alert("It's OK!!!!");
     }
-    else{
-        alert('Incorrect');
+    else
+    {
+        alert("It's wrong!!!!");
+    }
+
+    currentQuestionIndex++;
+
+    if (areQuestionsExhausted())
+    {
+        showCompletedMessage();
+    }
+    else
+    {
+        askCurrentQuestion();
+        inputField.value = "";
     }
 }
+
+function showCompletedMessage()
+{
+    document.getElementById('question').textContent = "You are done";
+}
+
+function areQuestionsExhausted()
+{
+    return currentQuestionIndex >= questions.length;
+}
+
+function askCurrentQuestion()
+{
+    askQuestion(questions[currentQuestionIndex]);
+}
+
+askCurrentQuestion();
