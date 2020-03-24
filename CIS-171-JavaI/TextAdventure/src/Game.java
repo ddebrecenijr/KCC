a7a4ccd6e2645000dbdc327378fe5e25b3a011e3
David Debreceni <davidjr@debreceni.net>
2020-03-23 20:39:09 -0500

    Update Game.java
    Finished starting room choices.
    Started working on altar room.

--- CIS-171-JavaI/TextAdventure/src/Game.java
+++ CIS-171-JavaI/TextAdventure/src/Game.java
@@ -12,38 +12,7 @@ public class Game {
     public static void main(String[] args)  {
         gameStart();
         introduction();
-
-        System.out.println("You see a door to the north.");
-        System.out.println();
-
-        System.out.println("What would you like to do?");
-        System.out.println("1: Go North");
-        System.out.println("2: Examine Skull");
-        System.out.println("3: Examine Stones");
-        System.out.println("4: Examine Branches");
-        System.out.println();
-
-        var choice = getUserInput.nextInt();
-
-        if(choice == 1){
-            // Go North
-        }
-        else if(choice == 2) {
-            // Examine Skull
-            examineSkull();
-        }
-        else if(choice == 3) {
-            // Examine Stones
-            examineStones();
-        }
-        else if(choice == 4) {
-            // Examine Branches
-            examineBranches();
-        }
-        else{
-            // Go back to room
-            startingRoom();
-        }
+        startRoomOptions();
     }
 
     private static void examineSkull() {
@@ -78,6 +47,11 @@ public class Game {
         System.out.println("Amongst some of the stones, you notice pieces of flint.");
         System.out.println();
 
+        // If we already have a flint, just go back to starting room.
+        if (checkIfExistsInInventory("flint")) {
+            startingRoom();
+        }
+
         // Make Choice
         System.out.println("What would you like to do?");
         System.out.println("1: Take flint");
@@ -96,9 +70,15 @@ public class Game {
     }
 
     private static void examineBranches() {
+        // Description
         System.out.println();
         System.out.println("The branches appear to be very dry.");
         System.out.println();
+
+        // If we already have a branch, just go back to starting room.
+        if (checkIfExistsInInventory("branch")) {
+            startingRoom();
+        }
 
         System.out.println("What would you like to do?");
         System.out.println("1: Take branch");
@@ -125,6 +105,112 @@ public class Game {
         if(!checkIfExistsInInventory("skull"))
             System.out.println("There is a skull atop a pile of bones.");
         System.out.println();
+
+        startRoomOptions();
+    }
+
+    private static void startRoomOptions() {
+        System.out.println("You see a door to the north.");
+        System.out.println();
+
+        System.out.println("What would you like to do?");
+        System.out.println("1: Go North");
+        System.out.println("2: Examine Stones");
+        System.out.println("3: Examine Branches");
+        if (!checkIfExistsInInventory("skull"))
+            System.out.println("4: Examine Skull");
+        System.out.println();
+
+        var choice = getUserInput.nextInt();
+
+        if(choice == 1){
+            // Go North
+            altarRoom();
+        }
+        else if(choice == 2) {
+            // Examine Stones
+            examineStones();
+        }
+        else if(choice == 3) {
+            // Examine Branches
+            examineBranches();
+        }
+        else if(choice == 4 && !checkIfExistsInInventory("skull")) {
+            // Examine Skull
+            examineSkull();
+        }
+        else{
+            // Go back to room
+            startingRoom();
+        }
+    }
+
+    private static void altarRoom() {
+        // Description
+        System.out.println();
+        System.out.println("You find yourself in a dark room without any visible exits.");
+        System.out.println("At its center lies that which looks like an altar.");
+        System.out.println();
+
+        System.out.println("What would you like to do?");
+        System.out.println("1: Examine Walls");
+        System.out.println("2: Examine Altar");
+        System.out.println("3: Go Back");
+        System.out.println();
+
+        var choice = getUserInput.nextInt();
+
+        if(choice == 1){
+            // Examine Walls
+            examineWalls();
+        }
+        else if(choice == 2) {
+            // Examine Altar
+//            examineAltar();
+        }
+        else if(choice == 3) {
+            // Go Back
+            startingRoom();
+        }
+        else{
+            // Go back to room
+            altarRoom();
+        }
+    }
+
+    private static void examineWalls() {
+        // Description
+        System.out.println();
+        // Do we have a torch?
+        if (checkIfExistsInInventory("torch")) {
+            System.out.println("Upon closer inpsection, you make out a depiction that appears to look like an altar");
+            System.out.println("with a skull on it.");
+        }
+        else {
+            System.out.println("There appears to be inscriptions, but it is too dark to read.");
+        }
+        System.out.println();
+
+        // Present Menu
+        System.out.println("What would you like to do?");
+        System.out.println("1: Go Back");
+
+        boolean playerHasTorchMaterials =
+                checkIfExistsInInventory("branch") && checkIfExistsInInventory("flint");
+
+        if (playerHasTorchMaterials) {
+            System.out.println("2: Craft a torch from branch and flint");
+        }
+
+        var choice = getUserInput.nextInt();
+
+        if (choice == 2 && playerHasTorchMaterials) {
+            // Craft a torch
+
+            examineWalls();
+        }
+
+        altarRoom();
     }
 
     private static boolean checkIfExistsInInventory(String item) {
@@ -133,6 +219,27 @@ public class Game {
                 return true;
         }
         return false;
+    }
+
+    private static void craftItem(String nameOfItemToCreate, String[] materials) {
+        // Check we have materials
+        for (int i = 0; i < materials.length; i++) {
+            if(!checkIfExistsInInventory(materials[i])) {
+                return;
+            }
+        }
+
+        // Remove materials from inventory
+        // ["branch", "skull", "flint"] 2
+        // Items 0, and 2
+        // ["skull"]
+
+
+
+
+        // Create Item
+        // ["skull", "torch", "flint"]
+
     }
 
     private static void gameStart() {
